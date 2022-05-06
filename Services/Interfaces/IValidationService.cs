bbd11f826aade63c89223968007b4db17778f865
felipepmoreno <felipepmoreno@gmail.com>
2022-05-05 22:53:32 -0300

    Add project files.

---
+++ Services/Interfaces/IValidationService.cs
@@ -0,0 +1,13 @@
+﻿using ServiceP2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceP2.Services.Interfaces
+{
+    public interface IValidationService
+    {
+        public bool ValidateInformations(P2BodyModel model);
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceP2.Models;
+using ServiceP2.Services;
+using ServiceP2.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ServiceP2
+{
+    [Route("api/p2/validate")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IValidationService _validationService;
+        public ValuesController(IValidationService validationService)
+        {
+            _validationService = validationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> P2ValidationAsync([FromBody] P2BodyModel model)
+        {
+           try
+           {
+                bool validated = _validationService.ValidateInformations(model);
+                return Ok(validated);
+           }
+           catch(Exception ex)
+           {
+                return StatusCode(500, ex.Message);     
+           }
+           
+        }
+
+    }
+}
+
