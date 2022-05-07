628e5b82f49b0be9d2206f8cef5d9b4bd4329e0b
felipepmoreno <felipepmoreno@gmail.com>
2022-05-07 14:54:20 -0300

    Implementação do metodo post para o service p3

--- ValuesController.cs
+++ ValuesController.cs
@@ -5,6 +5,8 @@ using ServiceP2.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,6 +18,7 @@ namespace ServiceP2
     public class ValuesController : ControllerBase
     {
         public IValidationService _validationService;
+        public HttpClient client = new HttpClient();
         public ValuesController(IValidationService validationService)
         {
             _validationService = validationService;
@@ -27,6 +30,16 @@ namespace ServiceP2
            try
            {
                 bool validated = _validationService.ValidateInformations(model);
+                if(validated)
+                    await client.PostAsync(
+                        "https://localhost:5002/api/p3/generate-token", 
+                        new StringContent(model.ToString(), Encoding.UTF8, "application/json")
+                     );
+                else
+                {
+                    return Ok("Parâmetros inválidos");
+                }
+
                 return Ok(validated);
            }
            catch(Exception ex)
